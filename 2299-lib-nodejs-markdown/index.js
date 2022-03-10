da5cb06338e1fe3cd9cd5cbe98724dfa179421be
edemuner <eduardood15@hotmail.com>
2022-03-10 17:54:18 -0300

    added an error treating for getFile function

--- 2299-lib-nodejs-markdown/index.js
+++ 2299-lib-nodejs-markdown/index.js
@@ -1,9 +1,16 @@
 const chalk = require('chalk');
 const fs = require('fs')
 
+function treatError(error){
+  throw new Error(chalk.red(error.code, 'File not found'))
+}
+
 function getFile(filePath){
   const encoding = 'utf-8'
   fs.readFile(filePath, encoding, (err, data) => {
+    if (err){
+      treatError(err)
+    }
     console.log(chalk.green(data))
   })
 }
