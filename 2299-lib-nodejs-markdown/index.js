5564bfd7661b040fccae2d530d3c2673f7b86f73
edemuner <eduardood15@hotmail.com>
2022-03-11 17:41:15 -0300

    replaced .match() for .exec() to group the results, and a while loop to iterate through the results and better organize data to show the selection

--- 2299-lib-nodejs-markdown/index.js
+++ 2299-lib-nodejs-markdown/index.js
@@ -1,29 +1,32 @@
 const chalk = require('chalk');
 const fs = require('fs')
 
-const text = "A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](http://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)"
 
 function extractLinks(text){
   const regex = /\[([^\]]*)\]\((https?:\/\/[^\s]*.[^\s]*)\)/gm;
-  const extractedLinks = text.match(regex)
-  return extractedLinks
+  const resultsAray = []
+  let temp
+
+  while((temp = regex.exec(text)) !== null){
+    resultsAray.push({ [temp[1]]: temp[2] })
+  }
+  return resultsAray
 }
 
 function treatError(error){
   throw new Error(chalk.red(error.code, 'File not found'))
 }
 
-console.log(extractLinks(text))
 
-// async function getFile(filePath){
-//   const encoding = 'utf-8'
-//   try{
-//     const text = await fs.promises.readFile(filePath, encoding)
-//     console.group(chalk.green(text))  
-//   } catch(error){
-//     treatError(error)
-//   }
-// }
+async function getFile(filePath){
+  const encoding = 'utf-8'
+  try{
+    const text = await fs.promises.readFile(filePath, encoding)
+    console.log(extractLinks(chalk.green(text)))  
+  } catch(error){
+    treatError(error)
+  }
+}
 
 // function getFile(filePath){
 //   const encoding = 'utf-8'
@@ -43,4 +46,4 @@ console.log(extractLinks(text))
 // }
 
 
-// getFile('./files/text.md')
+getFile('./files/text.md')
