e8cf241fad56cfcfe9424bc3373d0c7424467e35
edemuner <eduardood15@hotmail.com>
2022-03-11 17:27:05 -0300

    made an match function with regex to find links inside a text

--- 2299-lib-nodejs-markdown/index.js
+++ 2299-lib-nodejs-markdown/index.js
@@ -1,19 +1,29 @@
 const chalk = require('chalk');
 const fs = require('fs')
 
+const text = "A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](http://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)"
+
+function extractLinks(text){
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^\s]*.[^\s]*)\)/gm;
+  const extractedLinks = text.match(regex)
+  return extractedLinks
+}
+
 function treatError(error){
   throw new Error(chalk.red(error.code, 'File not found'))
 }
 
-async function getFile(filePath){
-  const encoding = 'utf-8'
-  try{
-    const text = await fs.promises.readFile(filePath, encoding)
-    console.group(chalk.green(text))  
-  } catch(error){
-    treatError(error)
-  }
-}
+console.log(extractLinks(text))
+
+// async function getFile(filePath){
+//   const encoding = 'utf-8'
+//   try{
+//     const text = await fs.promises.readFile(filePath, encoding)
+//     console.group(chalk.green(text))  
+//   } catch(error){
+//     treatError(error)
+//   }
+// }
 
 // function getFile(filePath){
 //   const encoding = 'utf-8'
@@ -33,4 +43,4 @@ async function getFile(filePath){
 // }
 
 
-getFile('./files/text.md')
+// getFile('./files/text.md')
