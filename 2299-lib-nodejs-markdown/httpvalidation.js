bebb40af8501527f1d2c6320be0e6dde36b7fb02
edemuner <eduardood15@hotmail.com>
2022-03-14 19:55:32 -0300

    made a generateUrlsArray function that returns a list of the links, extracted from the values of the received objects

--- 2299-lib-nodejs-markdown/httpvalidation.js
+++ 2299-lib-nodejs-markdown/httpvalidation.js
@@ -1,5 +1,10 @@
+function generateUrlsArray(linkArray){
+    console.log(linkArray)
+    return linkArray.map(linkObject => Object.values(linkObject).join())
+}
+
 function validateURLs(linkArray){
-    return linkArray
+    return linkArray.map(array => generateUrlsArray(array))
 }
 
 module.exports = validateURLs
