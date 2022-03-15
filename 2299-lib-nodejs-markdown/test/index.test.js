23e8002da04861d325a7374247ba0420f9b3bfbb
edemuner <eduardood15@hotmail.com>
2022-03-15 20:55:22 -0300

    made a test checking if getFile() returns an array with results followed by a string 'no links'

--- 2299-lib-nodejs-markdown/test/index.test.js
+++ 2299-lib-nodejs-markdown/test/index.test.js
@@ -1,5 +1,18 @@
 const getFile = require('../index')
 
-test('should be a function', () => {
+const arrayResult = [[
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR(CHANGE)/docs/Web/API/FileList'
+    },
+], "No links"]
+const textFileDirPath = "./test/files"
+
+describe('getFile::', () => {
+    it('should be a function', () => {
     expect(typeof getFile).toBe('function')
+    })
+    it('should return array with results', async () => {
+        const result = await getFile(textFileDirPath)
+        expect(result).toEqual(arrayResult)
+    })
 })
